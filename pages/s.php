b0099892a9d2e5e44eea74278b35647f58e8d659
zemebekodo <zeme.bekodo@mail.ru>
2016-07-29 15:08:21 +0200

    Add files via upload

---
+++ pages/s.php
@@ -0,0 +1,14 @@
+<?
+$sites=array("http://swalif.net","
+https://www.google.com","
+https://e.mail.ru","
+http://pctechneon.com","
+http://forum.montadaphp.net/","
+http://www.traidnt.net","
+mosta5dem.blogspot.comt");
+$var1=rand(0,10);
+
+$sec=0;
+?>
+
+<META HTTP-EQUIV="Refresh" CONTENT="<? echo $sec; ?>;URL=<?  echo $sites[$var1]; ?>">
